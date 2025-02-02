303e0776d08204cee75629af4ab643a29c2901e6
Jose Leon <leonj1@gmail.com>
2025-02-02 20:07:19 +0000

    FEAT shoot left and right

--- util/common.go
+++ util/common.go
@@ -4,13 +4,12 @@ import (
 	"math"
 )
 
-//CalculateDistance returns the distance between points x1,y1 and x2,y2
+// CalculateDistance returns the distance between points x1,y1 and x2,y2
 func CalculateDistance(x1, y1, x2, y2 int) float64 {
-	x := math.Pow((float64)(x2-x1), 2)
-	y := math.Pow((float64)(y2-y1), 2)
-
-	d := math.Sqrt(x + y)
-	return d
+	// Use Manhattan distance for grid-based movement
+	dx := math.Abs(float64(x2 - x1))
+	dy := math.Abs(float64(y2 - y1))
+	return dx + dy
 }
 
 // Notifier is an interface that can be implemented to recieve messages
